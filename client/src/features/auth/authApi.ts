6f08620708da40c7ade551ab0b9d49b4f3a6ad9b
Amritpal Singh <35212838+amritpal7@users.noreply.github.com>
2025-06-11 18:02:00 +0530

    prod_env upd

--- client/src/features/auth/authApi.ts
+++ client/src/features/auth/authApi.ts
@@ -1,7 +1,4 @@
-const endPoint =
-  import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_LOCAL_URL
-    : import.meta.env.VITE_PROD_URL;
+const endPoint = import.meta.env.VITE_PROD_URL;
 
 export const loginUser = async (credentials: {
   username: string;
